72117c4f6a59506c7220fae2d5cc75e6e1b3687b
lumingqiu <527468575@qq.com>
2019-09-08 13:56:14 +0800

    1

--- tp5/application/admin/controller/Index.php
+++ tp5/application/admin/controller/Index.php
@@ -68,7 +68,92 @@ class Index extends \app\common\controller
 
     public function app_upload()
     {
-        return $this->fetch();
+        $page = 1;
+        $page_size = 20;
+        $page_count = count(Db::table('cj_ad_app')->select());
+        $applist = Db::table('cj_ad_app')
+            ->order('app_id', 'desc');
+        if($this->request->param()){  
+            if($this->request->get("name")){
+                $name = $this->request->get("name");
+                $applist = $applist->where('app_name','like',"%".$name."%");
+            }
+            // if($this->request->get("chaname")){
+            //     $chaname = $this->request->get("chaname");
+            //     $applist = $applist->where('ad_name','like',"%".$des."%");
+            // }
+            if($this->request->get("des")){
+                $des = $this->request->get("des");
+                $applist = $applist->where('ad_name','like',"%".$des."%");
+            }
+            if($this->request->get("id")){
+                $id = $this->request->get("id");
+                $applist = $applist->where("app_id",$id);
+            }
+            if($this->request->param("page")){
+                $page = $this->request->param("page");
+            }
+            
+        }
+        $applist = $applist->limit(($page-1)*$page_size,$page_size);
+        $applist = $applist->select();
+        if($applist){
+            $applist = $applist;
+        }else{
+            $applist = [];
+        }
+        $pageinfo["page_count"] = $page_count;
+        $pageinfo["page_size"] = $page_size;
+        $pageinfo["page_index"] = $page;
+        $pageinfo["page_num"] = \ceil($page_count/$page_size);
+        $userlist = Db::table('cj_user')
+                           ->select();
+        return $this->fetch("",['applist'=>$applist,"pageinfo"=>$pageinfo,"userlist"=>$userlist]);
+        
+    }
+
+    public function app_info()
+    {
+        $arr = [];
+           
+        if($this->request->param()){
+            $do = $this->request->param("do");
+            $channel_activate = $this->request->param("channel_activate");
+            $app_id = $this->request->param("app_id");
+            $this->request->get(["do"=>$do,"app_id"=>$app_id]);
+            $app = "";
+            $userlist = "";
+            if($do == 'edit'){
+                if($app_id){
+                    $app = Db::table('cj_ad_app')
+                            ->where("app_id",$app_id)->select()[0];
+                }
+                
+                
+
+            }elseif($channel_activate){
+                $data = $this->request->post();
+                Db::table('cj_ad_app')->update($data);
+                return ["error"=>0,"message"=>"保存成功"];
+            }elseif($do == 'add'){
+                $data = $this->request->post();
+                if(isset($data['app_id'])){
+                    Db::table('cj_ad_app')->update($data);
+                } else{
+                    $app_id = Db::table('cj_ad_app')->insertGetId($data);
+                }          
+                return $this->redirect('app_info',["do"=>"edit","app_id"=>$app_id,"par_up"=>1] );
+                
+            }elseif($do=='del'){
+                Db::table('cj_ad_app')->where("app_id",$app_id)->update(["user_state"=>2]);
+                return  $this->fetch('',["par_up"=>1]);
+            }
+            $userlist = Db::table('cj_user')
+                           ->select();
+            $arr = ["appinfo"=>$app,"userlist"=>$userlist];
+            
+        }
+        return $this->fetch('',$arr);
     }
 
     public function userlist()
@@ -104,17 +189,20 @@ class Index extends \app\common\controller
 
     public function user(){
         $arr = [];
-
+        
         if($this->request->param()){
             $do = $this->request->param("do");
             
-            $user_id = $this->request->param("user_id");
+            $user_id = intval($this->request->param("user_id"));
             $this->request->get(["do"=>$do,"user_id"=>$user_id]);
             $user = "";
             if($do == 'edit'){
-                $user = Db::table('cj_user')
-                            ->where("user_id",$user_id)->select()[0];
+                if($user_id){
+                    $user = Db::table('cj_user')
+                    ->where("user_id",$user_id)->select()[0];
 
+                }
+               
             }elseif($do == 'add'){
                 $data = $this->request->post();
 
